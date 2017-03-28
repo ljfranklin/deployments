265e37557426334fb813a9e6668260a55b0c3102
Lyle Franklin <lylejfranklin@gmail.com>
2017-03-27 21:15:48 -0700

    First pass at using bosh-deployment

--- terraform/output.tf
+++ terraform/output.tf
@@ -2,24 +2,40 @@ output "gce_region" {
   value = "${var.gce_region}"
 }
 
-output "gce_zone" {
+output "zone" {
   value = "${var.gce_zone}"
 }
 
-output "network_name" {
+output "project_id" {
+  value = "${var.projectid}"
+}
+
+output "network" {
   value = "${google_compute_network.bosh.name}"
 }
 
-output "subnetwork_name" {
+output "subnetwork" {
   value = "${google_compute_subnetwork.bosh.name}"
 }
 
-output "deployments_bucket_name" {
-  value = "${google_storage_bucket.deployment-bucket.name}"
+output "internal_ip" {
+  value = "${google_compute_address.director.address}"
 }
 
-output "bosh_external_ip" {
+output "internal_cidr" {
+  value = "${google_compute_subnetwork.bosh.ip_cidr_range}"
+}
+
+output "internal_gw" {
+  value = "${cidrhost(google_compute_subnetwork.bosh.ip_cidr_range, 1)}"
+}
+
+output "external_ip" {
   value = "${google_compute_address.director.address}"
+}
+
+output "tags" {
+  value = ["bosh-internal", "bosh-external"]
 }
 
 output "concourse_external_ip" {
@@ -32,4 +48,8 @@ output "bosh_hostname" {
 
 output "concourse_hostname" {
   value = "${cloudflare_record.concourse-dns.hostname}"
+}
+
+output "deployments_bucket_name" {
+  value = "${google_storage_bucket.deployment-bucket.name}"
 }
